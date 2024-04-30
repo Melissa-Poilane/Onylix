09da86e175b75edb88bbc3e000b1fcb7dc0ca6dc
Melissa-Poilane <melissa.poilane@edu.univ-fcomte.fr>
2024-04-30 16:47:46 +0200

    commit initial

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import HeaderPage from './components/HeaderPage.vue'
+import FooterPage from './components/FooterPage.vue'
+</script>
+
+<template>
+  <HeaderPage />
+  <main>
+    <RouterView />
+  </main>
+  <FooterPage />
+</template>
