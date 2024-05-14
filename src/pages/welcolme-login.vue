e0c11c6516b4aab8e45ee48a95caada4332724a9
Melissa-Poilane <melissa.poilane@edu.univ-fcomte.fr>
2024-05-15 00:01:09 +0200

    chore: Update favicon and page title in index.html, and improve welcome screen layout in welcolme-login.vue

--- src/pages/welcolme-login.vue
+++ src/pages/welcolme-login.vue
@@ -1,10 +1,25 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
+
 <template>
-  <main class=" bg-violet-900 h-screen">
-<img src="/img/bvn/liquid-bvn.svg" alt="header décoratif" class="w-full relative">
-<div class="flex flex-col justify-center items-center mx-16 " >
-<img src="/img/bvn/mascotte-bvn.svg" alt="image de onyx"  >
-<h3>Bienvenue dans Onylix</h3>
+  <main class=" bg-violet-900">
+   <div class="row-span-9 grid grid-cols-1 grid-rows-[6fr_3fr]">
+<div class="row-start-1 row-span-5">
+    <img src="/img/bvn/liquid-bvn.svg" alt="header décoratif" class="w-full ">
+ <div class="flex justify-center mx-10">
+<img src="/img/bvn/mascotte-bvn.svg" alt="image de onyx" >
+</div>
+<div class="flex flex-col row-start-7 row-span-2 justify-center items-center text-center mx-10 pb-16">
+<h3 class="mb-3">Bienvenue dans Onylix</h3>
 <p>A partir d’aujourd’hui, nous comprendrons le sens de nos rêves !</p>
- </div> </main>
+
+<div class="flex justify-center py-5 w-full bg-gray-50 rounded-full mb-2 mt-7">
+  <RouterLink to="/inscription" >C’est parti</RouterLink>
+</div>
+<RouterLink to="/connexion" >Tu as déjà un compte ?</RouterLink>
+
+ </div> </div></div>
+</main>
   </template>
   
