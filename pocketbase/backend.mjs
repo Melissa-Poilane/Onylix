21bc0d29a9ab485020a62c8f5a9fc2bd6a43e8e2
Melissa-Poilane <melissa.poilane@edu.univ-fcomte.fr>
2024-05-25 00:19:46 +0200

    uynfny

--- pocketbase/backend.mjs
+++ pocketbase/backend.mjs
@@ -1,10 +1,81 @@
-import PocketBase from 'pocketbase' ;
-const pb = new PocketBase('http://127.0.0.1:8090') ;
+import PocketBase from 'pocketbase'
 
-//Remonter les donnee d'un user en passant son id en param depuis reves
-export async function allDreamUser() {
-    const record =  await pb.collection('maison').getFullList(
-        { expand: 'agent',
-    }) ;
-    return record ;
-}
+const pb = new PocketBase('http://127.0.0.1:8090')
+/*
+export async function getDream() {
+    const records = await pb.collection('reves').getFullList()
+    return records
+  }
+  
+
+  
+  try {
+    //debugger
+    const records = await getUser()
+    console.log(records)
+  } catch (e) {
+    console.error(e)
+  }
+
+  try {
+    //debugger
+    const records = await getDreamOnline()
+    console.log(records)
+  } catch (e) {
+    console.error(e)
+  }
+  export async function oneDream(id) {
+    const oneRecord = await pb.collection('reves').getOne(id)
+  
+    return oneRecord
+  }
+  try {
+    //debugger
+    const records = await oneDream('peucwbg2hw7gwm0')
+    console.log(records)
+  } catch (e) {
+    console.error(e)
+  }
+
+  export async function getDreamOnline() {
+    // Modifier la requête pour filtrer jusqu'à la date du jour actuel
+    const filteredresults = await pb.collection('reves').getFullList( {
+      filter: ` online = true `,
+      sort: '+Date'
+    })
+    return filteredresults
+  }
+*/
+
+  async function getDreamsWithAuthors() {
+    try {
+      const records = await pb.collection('reves').getFullList({
+        expand: 'users',
+      });
+      return records;
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  
+  try {
+    const records = await getDreamsWithAuthors();
+    console.log(records);
+  } catch (e) {
+    console.error(e);
+  }
+  
+/*
+  async function getUser() {
+    const records = await pb.collection('users').getFullList()
+    return records
+  } 
+  
+  try {
+    //debugger
+    const records = await getUser()
+    console.log(records)
+  } catch (e) {
+    console.error(e)
+  }
+  */
